98130bbc0602a2c310aed1d62d858459f55de1ed
Abel España Carrera <abel.espana@soprasteria.com>
2021-11-03 11:56:53 +0100

    Testing the event is fired

--- test/components/Button.test.js
+++ test/components/Button.test.js
@@ -1,5 +1,6 @@
 import { html } from 'lit';
 import { fixture, expect } from '@open-wc/testing';
+import { oneEvent } from '@open-wc/testing-helpers';
 
 import '../../src/components/Button.js';
 
@@ -14,5 +15,13 @@ describe('Testing custom component button', () => {
 
   it('should render a button with the buttonStr "left" as value', () => {
     expect(appButton.value).to.equal(buttonStr);
+  });
+
+  it('should emit a custom event with the buttonStr property', async () => {
+    const listener = oneEvent(appButton, 'click');
+    const node = appButton.shadowRoot.querySelector('button');
+    node.click();
+    const event = await listener;
+    expect(event).to.exist;
   });
 });
